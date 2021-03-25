1eb3f637bb507c2aae50d93b6d8423e36b798842
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-03-24 23:14:04 -0400

    Add clear display functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useReducer} from 'react';
 import reducer, {initialState} from './reducers/index';
-import {addOne, applyNumber, changeOperation} from './actions/index';
+import {addOne, applyNumber, changeOperation, clearDisplay} from './actions/index';
 
 import './App.css';
 
@@ -65,7 +65,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={() => {dispatch(clearDisplay())}}/>
             </div>
 
           </form>
